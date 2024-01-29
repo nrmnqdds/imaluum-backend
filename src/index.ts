8c7cd267687672e419288e4073e6caea7ef0e75a
nrmnqdds <quddusnuriman@gmail.com>
2024-01-29 17:13:34 +0800

    :wrench: configure for deno

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
 import { ImaluumLogin } from "./scraper/login";
 import { GetSchedule } from "./scraper/schedule";
-import { setCookie } from "hono/cookie";
-import { cors } from "hono/cors";
+import { setCookie } from "https://deno.land/x/hono@v3.12.8/helper/cookie/index.ts";
+import { cors } from "https://deno.land/x/hono@v3.12.8/middleware/cors/index.ts";
+import { prettyJSON } from "https://deno.land/x/hono@v3.12.8/middleware/pretty-json/index.ts"
 
 const app = new Hono();
 
 app.use("*", cors());
+app.use("*", prettyJSON());
 
 app.get("/", (c) => {
   return c.json({
@@ -71,7 +72,4 @@ app.get("/schedule", async (c) => {
 const port = 3000;
 console.log(`Server is running on port ${port}`);
 
-serve({
-  fetch: app.fetch,
-  port,
-});
+Deno.serve(app.fetch);
