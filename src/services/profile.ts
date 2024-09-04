2db67909bb11bee9b8e10d08b5a79071d76c7ba6
nrmnqdds <quddusnuriman@gmail.com>
2024-09-04 21:05:10 +0800

    :bug: fix profile again

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -12,39 +12,37 @@ export async function GetProfile(c: string) {
 		});
 
 		const root = parse(response.body);
-		// const _name = root.querySelector(
-		//   ".row .col-md-12 .box.box-default .panel-body.row .col-md-4[style='text-align:center; padding:10px; floaf:left;'] h4[style='margin-top:1%;']",
-		// );
 
 		const _name = root.querySelector(
-			".navbar-custom-menu ul.nav.navbar-nav li.dropdown.user.user-menu span.hidden-xs",
+			".row .col-md-12 .box.box-default .panel-body.row .col-md-4[style='text-align:center; padding:10px; floaf:left;'] h4[style='margin-top:1%;']",
 		);
-		// const _matricNo = root.querySelector(
-		// 	".row .col-md-12 .box.box-default .panel-body.row .col-md-4[style='margin-top:1%;'] h4",
-		// );
 
-		// if (!_name || !_matricNo) {
-		// 	// Check if the selectors were found
-		// 	throw new Error("Selectors not found on the page.");
-		// }
+		const _matricNo = root.querySelector(
+			".row .col-md-12 .box.box-default .panel-body.row .col-md-4[style='margin-top:3%;'] h4",
+		);
 
-		if (!_name) {
+		if (!_name?.textContent || !_matricNo?.textContent) {
+			console.log("_name: ", _name?.textContent.trim());
+			console.log("_matricNo: ", _matricNo?.textContent);
+
+			// Check if the selectors were found
 			throw new Error("Selectors not found on the page.");
 		}
 
 		const name = _name.textContent?.trim();
-		// const matricNo = _matricNo.textContent?.trim().split("|")[0].trim();
-		// const imageURL = `https://corsproxy.io/?https://smartcard.iium.edu.my/packages/card/printing/camera/uploads/original/${matricNo}.jpeg`;
+		const matricNo = _matricNo.textContent?.trim().split("|")[0].trim();
+		const imageURL = `https://corsproxy.io/?https://smartcard.iium.edu.my/packages/card/printing/camera/uploads/original/${matricNo}.jpeg`;
 
 		return {
 			success: true,
 			data: {
-				// imageURL,
+				imageURL,
 				name,
-				// matricNo,
+				matricNo,
 			},
 		};
 	} catch (err) {
+		console.log(err);
 		throw new Error("Failed to fetch user profile");
 	}
 }
